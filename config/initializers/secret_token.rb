20029f1b282dd881c501ad1aa112752a2425751d
keisuke <furukawa.ke15uke@gmail.com>
2016-03-14 11:58:02 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timemoney::Application.config.secret_token = 'd22dd5f80e2d1e3c470f1387f7ed5f2b9100a1d72cc1cf01c6492660941e1614eac3be75e17a5f992cd05278cfb700cd9f3517828fafc71f52a948a076f1371d'
